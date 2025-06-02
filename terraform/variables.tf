fa425c2f314d5e56c713a8719b16b9940b09be27
Morgan Davis <nivecher@users.noreply.github.com>
2025-06-02 05:04:47 +0000

    refactor: remove unused Route 53 zone ID variable from Terraform configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -42,8 +42,3 @@ variable "domain_name" {
   description = "The domain name for the ACM certificate"
   type        = string
 }
-
-variable "route53_zone_id" {
-  description = "The Route 53 hosted zone ID for DNS validation"
-  type        = string
-}
